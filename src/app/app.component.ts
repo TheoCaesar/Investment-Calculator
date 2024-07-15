711761d01d180a1a165cf9f4ad40b5def540f222
TheoCaesar <theocaesar7@gmail.com>
2024-07-14 17:48:15 -0700

    header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
+import { HeaderComponent } from './header/header.component';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [HeaderComponent,],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
