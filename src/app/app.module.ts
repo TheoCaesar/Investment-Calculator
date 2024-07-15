78d6841542f8818b70827081484f00ac54add783
TheoCaesar <theocaesar7@gmail.com>
2024-07-14 21:53:10 -0700

    nested modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
-import { InvestmentData } from './investment-data.model';
-import { FormsModule } from '@angular/forms';
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { InvestmentResultsComponent } from './investment-results/investment-results.component';
-import { UserInputComponent } from './user-input/user-input.component';
+import { UserModule } from "./user-input/user.module";
 
 @NgModule({
   declarations:[
     AppComponent,
     HeaderComponent,
-    UserInputComponent,
     InvestmentResultsComponent
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, UserModule ],
   bootstrap: [AppComponent]
 })
 export class AppModule{
---
+++ src/app/user-input/user.module.ts
@@ -0,0 +1,12 @@
+import { FormsModule } from '@angular/forms';
+import { UserInputComponent } from "./user-input.component";
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [UserInputComponent],
+  imports: [FormsModule],
+  exports: [UserInputComponent]
+})
+export class UserModule{
+
+}
