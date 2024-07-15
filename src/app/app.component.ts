82195d667d9218f6e082ce7d881337ad9dcde500
TheoCaesar <theocaesar7@gmail.com>
2024-07-14 17:56:56 -0700

    user-input component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { HeaderComponent } from './header/header.component';
 import { Component } from '@angular/core';
+import { UserInputComponent } from './user-input/user-input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent,],
+  imports: [HeaderComponent,UserInputComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
